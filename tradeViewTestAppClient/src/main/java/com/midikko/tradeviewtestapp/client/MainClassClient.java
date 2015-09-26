783f69f135413fcefcf3b27835f92924ea8fd4d0
midikko <potapov.of@gmail.com>
2015-09-26 22:29:55 +0300

    console io

--- tradeViewTestAppClient/src/main/java/com/midikko/tradeviewtestapp/client/MainClassClient.java
+++ tradeViewTestAppClient/src/main/java/com/midikko/tradeviewtestapp/client/MainClassClient.java
@@ -8,6 +8,7 @@ package com.midikko.tradeviewtestapp.client;
 import com.midikko.tradeviewtestapp.messages.GetFileRequest;
 import com.midikko.tradeviewtestapp.messages.GetFileResponse;
 import com.midikko.tradeviewtestapp.messages.GetFilesListRequest;
+import com.midikko.tradeviewtestapp.messages.GetFilesListResponse;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -24,20 +25,31 @@ public class MainClassClient {
     public static final int SERVER_PORT = 25569;
 
     public static void main(String[] args) {
-        Socket socket = null;
+       Socket socket = null;
         try {
-            System.out.println("Client on the way!");
-            socket = new Socket("localHost", SERVER_PORT);
-            System.out.println("Connected");
+            //TODO change all this sout to printf
+            System.out.println("Client is ready to be used");
+            System.out.println("Please enter host and port as according to the sample \"host:port\"");
+            String hostPort = System.console().readLine();
+            String[] inputs = hostPort.split(":");
+            socket = new Socket(inputs[0], Integer.parseInt(inputs[1]));
+            System.out.println("Successfully connected to " + hostPort);
             SocketHolder socketHolder = new SocketHolder(socket);
             GetFilesListRequest message = new GetFilesListRequest();
             System.out.println("Object to be written = " + message);
             socketHolder.sendMessage(message);
-            System.out.println("Read response :: " + socketHolder.readMessage());
-            socketHolder.sendMessage(new GetFileRequest(1));
-            GetFileResponse response = (GetFileResponse) socketHolder.readMessage();
-            System.out.println("Read response on get file :: " + response.getStatus());
-            if(response.getStatus()==1){
+            GetFilesListResponse fileListResponse = (GetFilesListResponse) socketHolder.readMessage();
+            System.out.println("server at this time contains the following files:");
+            int selector=0;
+            for(String name : fileListResponse.getNames()){
+                System.out.println(selector++ +". " + name);
+            }
+            System.out.println("Please select the number of file you wish to download");
+            int selectedFile = Integer.parseInt(System.console().readLine());
+            socketHolder.sendMessage(new GetFileRequest(selectedFile));
+            GetFileResponse getFileResponse = (GetFileResponse) socketHolder.readMessage();
+            System.out.println("Read response on get file :: " + getFileResponse.getStatus());
+            if(getFileResponse.getStatus()==1){
                 socketHolder.readFile();
             }
         } catch (IOException ex) {
