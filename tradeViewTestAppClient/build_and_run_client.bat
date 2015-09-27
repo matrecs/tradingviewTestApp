452f44aa4701663ff2a96265c0fb50260d777a07
midikko <potapov.of@gmail.com>
2015-09-28 02:46:38 +0300

    missed .bat

---
+++ tradeViewTestAppClient/build_and_run_client.bat
@@ -0,0 +1,15 @@
+#!/bin/bash          
+md build
+echo "1. build directory created"
+javac -d ./build -sourcepath src/main/java/ /home/midikko/git/tradingviewTestApp/tradeViewTestAppClient/src/main/java/com/midikko/tradeviewtestapp/client/MainClassClient.java
+echo "2. src compiled"
+cd build
+jar cfe client.jar com.midikko.tradeviewtestapp.client.MainClassClient com/*
+echo "3. jar builded"
+move client.jar ../client.jar
+cd ..
+rmdir build /s /q
+echo "================="
+java -jar client.jar
+
+$SHELL
---
+++ tradeViewTestAppServer/build_and_run_client.bat
@@ -0,0 +1,15 @@
+#!/bin/bash          
+md build
+echo "1. build directory created"
+javac -d ./build -sourcepath src/main/java/ src/main/java/com/midikko/tradeviewtestapp/server/MainClassServer.java
+echo "2. src compiled"
+cd build
+jar cfe server.jar com.midikko.tradeviewtestapp.server.MainClassServer com/*
+echo "3. jar builded"
+move server.jar ../server.jar
+cd ..
+rmdir build /s /q
+echo "================="
+java -jar server.jar
+
+$SHELL
